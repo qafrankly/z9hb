289ae05b4e4369b6b3ae984d0c4e43bbb73487ac
qafrankly <31970630+qafrankly@users.noreply.github.com>
2018-02-04 14:10:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom'
+
+class RandomTest extends Component {
+  static PropTypes = {
+    h1: PropTypes.string,
+    h2: PropTypes.string,
+  }
+  render(){
+    let {
+      h2 = 'Heba is testing react-websocket',
+    } = this.props;
+
+    let {
+      flux, config
+    } = this.context;
+
+    return (
+      <div>
+        <h2>{h2}</h2>
+      </div>
+    );
+  }
+}
+
+export default RandomTest;
